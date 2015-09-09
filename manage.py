ed5440f55e83b13b3d2494e0eb96e1f596d4d3fe
Luis Felipe Mendivelso Osorio <ing.felipe.mendivelso@gmail.com>
2015-09-09 15:57:10 -0500

    Asignación de Nombre a la aplicación "Smarttools".

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StrifeMyVideo.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smarttools.settings")
 
     from django.core.management import execute_from_command_line
 
